57b720a7a4fdd86eab341f6f9a1ca8530b6bde18
zturtledog <71353802+zturtledog@users.noreply.github.com>
2021-10-03 16:15:57 -0700

    import

---
+++ src/main/java/net/twallowhavenstudios/extraadditions/itemgroup/ProssesingItemGroup.java
@@ -0,0 +1,35 @@
+
+package net.twallowhavenstudios.extraadditions.itemgroup;
+
+import net.twallowhavenstudios.extraadditions.item.TabiconprossesingItem;
+import net.twallowhavenstudios.extraadditions.ExtraAdditionsModElements;
+
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+
+@ExtraAdditionsModElements.ModElement.Tag
+public class ProssesingItemGroup extends ExtraAdditionsModElements.ModElement {
+	public ProssesingItemGroup(ExtraAdditionsModElements instance) {
+		super(instance, 2);
+	}
+
+	@Override
+	public void initElements() {
+		tab = new ItemGroup("tabprossesing") {
+			@OnlyIn(Dist.CLIENT)
+			@Override
+			public ItemStack createIcon() {
+				return new ItemStack(TabiconprossesingItem.block);
+			}
+
+			@OnlyIn(Dist.CLIENT)
+			public boolean hasSearchBar() {
+				return false;
+			}
+		};
+	}
+	public static ItemGroup tab;
+}
